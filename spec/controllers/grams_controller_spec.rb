f85f78a044e3e622b67faed0f30e4eb64ebe24c3
Willie Broder <willie.broder@gmail.com>
2017-02-12 19:33:48 +0000

    Gram model added

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
-  describe "grams#index action" do
-    it "should successfully show the page" do
+  describe "#index" do
+    it "shows the page" do
       get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "#new" do
+    it "shows the new form page" do
+      get :new
       expect(response).to have_http_status(:success)
     end
   end
