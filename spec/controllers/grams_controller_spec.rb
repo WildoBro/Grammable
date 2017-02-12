b7c530fffdd22657fe6d2034887b13937e4d5f7b
Willie Broder <willie.broder@gmail.com>
2017-02-12 21:08:11 +0000

    Test Validations

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -9,22 +9,54 @@ RSpec.describe GramsController, type: :controller do
   end
 
   describe "#new" do
+    it "requires a logged in user" do 
+      get :new
+      expect(response).to redirect_to new_user_session_path
+    end
+
     it "shows the new form page" do
+      user = User.create(
+        email:                    'fakeuseremail@gmail.com',
+        password:                 'securePassword',
+        password_confirmation:    'securePassword'
+      )
+      sign_in user
+
       get :new
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "#create" do
+    it "requires a logged in user" do 
+      get :create, gram: {message: 'Hello!'}
+      expect(response).to redirect_to new_user_session_path
+    end
+
     it "create new gram in the database" do
+      user = User.create(
+        email:                    'fakeuseremail@gmail.com',
+        password:                 'securePassword',
+        password_confirmation:    'securePassword'
+      )
+      sign_in user
+      
       post :create, gram: {message: 'Hello!'}
       expect(response).to redirect_to root_path
 
       gram = Gram.last
       expect(gram.message).to eq("Hello!")
+      expect(gram.user).to eq(user)
       end
 
     it "validates entries" do
+      user = User.create(
+        email:                    'fakeuseremail@gmail.com',
+        password:                 'securePassword',
+        password_confirmation:    'securePassword'
+      )
+      sign_in user
+
       post :create, gram: {message: ' '}
       expect(response).to have_http_status(:unprocessable_entity)
       expect(Gram.count).to eq 0
